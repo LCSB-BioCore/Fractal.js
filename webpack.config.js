0e438961b0520f67cbf112b5a33ca9eebb182c5e
Sascha Herzinger <sascha.herzinger@uni.lu>
2017-03-16 10:09:02 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack')
+const path = require('path')
+
+const config = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './lib'),
+    library: 'fractal',
+    filename: 'fractal.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  }
+}
+
+module.exports = config
