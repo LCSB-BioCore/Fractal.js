415cc3fff78b6493ce2207f95885337a1213f05d
Sascha Herzinger <sascha.herzinger@uni.lu>
2017-03-16 15:09:32 +0100

    Beginning to implement request service

--- src/services/requests.js
+++ src/services/requests.js
@@ -1,3 +1,40 @@
-/**
- * Created by sascha on 16/03/2017.
- */
+import axios from 'axios'
+
+export default class {
+  _validateHandler (handler) {
+
+  }
+
+  _validateServerURL (thisServerURL) {
+
+  }
+
+  _validateAuth (thisAuth) {
+
+  }
+
+  constructor (handler, thisBaseURL, thisAuth, fractalisBaseURL) {
+    this._validateHandler(handler)
+    this._validateServerURL(thisBaseURL)
+    this._validateAuth(thisAuth)
+    this._validateServerURL(fractalisBaseURL)
+
+    this.handler = handler
+    this.thisBaseURL = thisBaseURL
+    this.thisAuth = thisAuth
+    this.fractalisBaseURL = fractalisBaseURL
+  }
+
+  loadData (descriptors) {
+    axios.post('/data', {
+      handler: this.handler,
+      server: this.server,
+      auth: this.auth,
+      descriptors,
+    }).then(response => {
+      console.log(response)
+    }).catch(error => {
+      console.log(error)
+    })
+  }
+}
